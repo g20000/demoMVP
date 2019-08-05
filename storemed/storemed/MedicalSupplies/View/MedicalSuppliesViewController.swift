b1b0659d2d57b67e66119cffd7f234d9ec200eeb
Andrew Bobkov <pragmus2@gmail.com>
2019-08-05 19:58:14 +0300

    Item view is created

---
+++ storemed/storemed/MedicalSupplies/View/MedicalSuppliesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MedicalSuppliesViewController.swift
+//  storemed
+//
+//  Created by pragmus on 02/08/2019.
+//  Copyright © 2019 pragmus. All rights reserved.
+//
+
+import UIKit
+
+class MedicalSuppliesViewController: UIViewController, MedicalSuppliesView, UITableViewDelegate, UITableViewDataSource {
+    
+
+    var presenter = MedicalSuppliesPresenter()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "Cell")!
+    }
+
+}
